73b05273ab51bf0f1d1a8397d07268ea6179bf16
your username <69278240+shadiaduery@users.noreply.github.com>
2021-01-08 21:39:30 -0800

    added screen shot picture

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -14,52 +14,44 @@
 var allData = data;
 
 // Viewing the available data from the data.js
-console.log(allData);
+// console.log(allData);
 
 // Creating Variables
-var $tbody = d3.select("tbody");
+var tableBody = d3.select("tbody");
 var button = d3.select("#filter-btn");
 var FormInputDate = d3.select("#datetime");
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
 
 // Inputing the data into the HTML
-var inputData = function (fillTable){
-    fillTable.forEach(UFOEvents=> {
-        var row = $tbody.append("tr");
-        columns.forEach(column => row.append("td").text(UFOEvents[column])
-        )
-    });
+var inputData = function (fillTable) {
+  fillTable.forEach(UFOEvents => {
+    var row = tableBody.append("tr");
+    columns.forEach(column => row.append("td").text(UFOEvents[column])
+    )
+  });
 }
 inputData(allData);
 
-// Creating an Event Listener for the Button
-// Setting up the Filter Button for Date and City
+// Creating an Event Listener to filter for date on the click of the button
 button.on("click", () => {
 
-    d3.event.preventDefault();
-    
+  d3.event.preventDefault();
 
-    var inputDate = FormInputDate.property("value").trim();
-    // console.log(inputDate)
-    
-    var filterDate = allData.filter(allData => allData.datetime === inputDate);
-    
+  var inputDate = FormInputDate.property("value");
+  // console.log(inputDate)
 
-    $tbody.html("");
+  var filterDate = allData.filter(allData => allData.datetime === inputDate);
 
-    let response = {
-        filterDate
-    }
+  tableBody.html("");
 
+  var response = {filterDate}
 
-    if(response.filterDate.length !== 0) {
-        inputData(filterDate);
-    }
+  if (response.filterDate.length !== 0) {
+    inputData(filterDate);
+  }
 
-    // Top if only works for filtering the date
-    
-        else {
-            $tbody.append("tr").append("td").text("No Sightings Here...Move On...");
-        }
+  else {
+    tableBody.append("tr").append("td").text("No UFO Sightings on this date");
+  }
 })
