2fa2f13ac242114102c031b71206a34930da6427
your username <69278240+shadiaduery@users.noreply.github.com>
2021-01-09 14:05:05 -0800

    added bonus answer

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,42 +1,75 @@
-// from data.js
-var tableData = data;
-console.log(tableData)
+// Using the UFO dataset provided in the form of an array of 
+// JavaScript objects, write code that appends a table to your
+// web page and then adds new rows of data for each UFO sighting.
 
-// YOUR CODE HERE!
+// Make sure you have a column for 
+// `date/time`, `city`, `state`, `country`, `shape`, and `comment` 
+// at the very least.
 
-// Select the button
-var filterButton = d3.select("#filter-btn");
+// * Use a date form in your HTML document and write JavaScript code 
+// that will listen for events and search through the `date/time` 
+// column to find rows that match user input.
 
-function onSubmit(event) {
-    event.preventDefault()
+// Starter Code
+var allData = data;
 
-    d3.select("#ufo-table-body").selectAll("tr").remove();
+// Viewing the available data from the data.js
+// console.log(allData);
 
-  // Select the input element and get the raw HTML node
-    var datetimeInput = d3.select("#datetime");
-
-  // Get the value property of the input element
-    var datetimeValue = datetimeInput.property("value");
-
-    console.log(typeof datetimeValue);
-
-
-    console.log(sitings);
-
-    var filteredData = sitings.filter(siting => siting.datetime === datetimeValue);
- 
-    console.log(filteredData);
-    filteredData.forEach(addRow);
- 
-};
+// Creating Variables
+var tableBody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var FormInputDate = d3.select("#datetime");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
 
-function addRow(siting) {
-    var tbody = d3.select("#ufo-table-body");
-    var row = tbody.append("tr")
-    var values = Object.values(siting)
-    console.log(values)
-    values.forEach( (value) => {
-        row.append("td").text(value);
-    })
-};
+// Inputing the data into the HTML
+var inputData = function (fillTable) {
+  fillTable.forEach(UFOEvents => {
+    var row = tableBody.append("tr");
+    columns.forEach(column => row.append("td").text(UFOEvents[column])
+    )
+  });
+}
+inputData(allData);
+
+// Creating an Event Listener to filter for date on the click of the button
+button.on("click", () => {
+
+  d3.event.preventDefault();
+
+  var inputDate = FormInputDate.property("value");
+  var inputCity = FormInputDate.property("value");
+  var inputState = FormInputDate.property("value");
+  var inputCountry = FormInputDate.property("value");
+  // console.log(inputDate)
+
+  var filterDate = allData.filter(allData => allData.datetime === inputDate);
+  var filterCity = allData.filter(allData => allData.city === inputCity);
+  var filterState = allData.filter(allData => allData.state === inputState);
+  var filterCountry = allData.filter(allData => allData.country === inputCountry);
+
+  tableBody.html("");
+
+  var response1 = {filterDate}
+  var response2 = {filterCity}
+  var response3 = {filterState}
+  var response4 = {filterCountry}
+
+  if (response1.filterDate.length !== 0) {
+    inputData(filterDate);
+  }
+  else if (response2.filterCity.length !== 0) {
+    inputData(filterCity);
+  }
+  else if (response3.filterState.length !== 0) {
+    inputData(filterState);
+  }
+  else if (response4.filterCountry.length !== 0) {
+    inputData(filterCountry);
+  }
+
+  else {
+    tableBody.append("tr").append("td").text("No UFO Sightings on this date");
+  }
+})
